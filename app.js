30dc7b52c73aae8fa44d530f95603c871e64e904
Tim Petrvalsky <tpet55@yahoo.com>
2021-03-16 12:58:34 -0700

    Fix merge conflicts

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'static')));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+// app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 const PORT = 7000;
 
 connectDB();
