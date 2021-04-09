0c9fa362ee8f5bcb4e6d5810f8b978008258eeb1
Tim Petrvalsky <tpet55@yahoo.com>
2021-04-09 12:10:18 -0700

    sScrape data

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'static')));
 // app.use(bodyParser.json({ limit: '50mb' }));
 // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
-const PORT = 7000;
+const PORT = 8500;
 
 connectDB();
 
